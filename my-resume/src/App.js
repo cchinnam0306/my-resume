59277d53dc314067114f88e101c18350d466d0e8
cchinnam0306 <141535384+cchinnam0306@users.noreply.github.com>
2023-08-12 17:59:33 -0700

    basic push

--- my-resume/src/App.js
+++ my-resume/src/App.js
@@ -4,7 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <header>
+        <h1>Chakradhar Chinnam</h1>
+        <p>DevOps Engineer</p>
+      </header>
+      <section id="summary">
+        <h2>Summary</h2>
+        <p>
+        Highly motivated and experienced software engineer with a strong background in Python development, Docker containerization, and continuous integration/continuous delivery (CI/CD) processes. Proficient in using Git for version control, Jenkins for CI/CD pipeline creation, and Ansible for infrastructure automation. Adequate knowledge in PowerShell and Shell scripting. A problem-solver with a passion for technology and a commitment to delivering high-quality and scalable software solutions. Seeking a challenging role in a dynamic organization where I can utilize my technical skills and experience to drive innovation and growth.
+        </p>
+      </section>
+      <section id="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>Git</li>
+          <li>Jenkins</li>
+          <li>Docker</li>
+          <li>Ansible</li>
+          <li>Python</li>
+          <li>Powershell</li>
+        </ul>
+      </section>
+      <section id="contact">
+        <h2>Contact Information</h2>
+        <p>Email: cc5155@outlook.com</p>
+        <p>LinkedIn: www.linkedin.com/in/chakradharchinnam</p>
+      </section>
     </div>
   );
 }
